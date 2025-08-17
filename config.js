6abcc46cf1b28f8d16a613cfc849b23d30739533
big846 <BIGWINNER30@icloud.com>
2025-08-17 22:47:34 +0000

    Update config.js

--- config.js
+++ config.js
@@ -5,39 +5,39 @@ const { color } = require('./lib/myfunc');
 const toBool = (value) => value === "true";
 
 
-global.owner = process.env.OWNER_NUMBER;
-global.nomerowner = process.env.OWNER_NUMBERS;
+global.owner = {"2250716961826"}process.env.OWNER_NUMBER;
+global.nomerowner ={"𝑻𝑯𝑬 𝑩𝑰𝑮𝑾𝑰𝑵𝑵𝑬𝑹 㟼"} process.env.OWNER_NUMBERS;
 
 global.menu_image = process.env.MENU_IMAGE;
 
-global.ANTI_TEMU = toBool(process.env.ANTI_TEMU);
-global.ANTI_TAG = toBool(process.env.ANTI_TAG);
+global.ANTI_TEMU = true(process.env.ANTI_TEMU);
+global.ANTI_TAG = true (process.env.ANTI_TAG);
 
-global.bot_name = process.env.BOT_NAME;
+global.bot_name ={𝑩𝑰𝑮𝑾𝑰𝑵𝑵𝑬𝑹㟼} process.env.BOT_NAME;
 
-global.public = toBool(process.env.PUBLIC);
+global.public = false (process.env.PUBLIC);
 
 
-global.packname = process.env.PACK_NAME;
+global.packname ={"𝑻𝑯𝑬 𝑩𝑰𝑮𝑾𝑰𝑵𝑵𝑬𝑹 㟼"} process.env.PACK_NAME;
 global.author = process.env.AUTHOR;
-global.ANTI_DELETE = toBool(process.env.ANTI_DELETE);
-global.ANTI_CALL = toBool(process.env.ANTI_CALL);
+global.ANTI_DELETE = false (process.env.ANTI_DELETE);
+global.ANTI_CALL = false (process.env.ANTI_CALL);
 
 
-global.unavailable = toBool(process.env.UNAVAILABLE);
-global.available = toBool(process.env.AVAILABLE);
-global.autoreadmessages = toBool(process.env.AUTO_READ_MESSAGES);
-global.chatbot = toBool(process.env.CHATBOT);
-global.autoreact = toBool(process.env.AUTO_REACT);
-global.autoTyping = toBool(process.env.AUTO_TYPING);
-global.autoViewStatus = toBool(process.env.AUTO_STATUS_VIEW);
-global.autoStatusReact = toBool(process.env.AUTO_STATUS_REACT);
-global.welcome = toBool(process.env.WELCOME);
-global.anticall = toBool(process.env.ANTI_CALL);
-global.autobio = toBool(process.env.AUTO_BIO);
+global.unavailable = false(process.env.UNAVAILABLE);
+global.available = true (process.env.AVAILABLE);
+global.autoreadmessages = false (process.env.AUTO_READ_MESSAGES);
+global.chatbot = true (process.env.CHATBOT);
+global.autoreact = false (process.env.AUTO_REACT);
+global.autoTyping = false (process.env.AUTO_TYPING);
+global.autoViewStatus = true (process.env.AUTO_STATUS_VIEW);
+global.autoStatusReact = false (process.env.AUTO_STATUS_REACT);
+global.welcome = false (process.env.WELCOME);
+global.anticall = false (process.env.ANTI_CALL);
+global.autobio = false (process.env.AUTO_BIO);
 
 
-global.prefix = process.env.PREFIX;
+global.prefix ={"´"} process.env.PREFIX;
 
 
 let file = require.resolve(__filename);
